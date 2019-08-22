3afa2fe635d6ce0005b6aab0aef15235eb3c2726
Rui Azevedo <ruihfazevedo@gmail.com>
2019-08-22 22:50:14 +0000

    eager

--- src/curry.h
+++ src/curry.h
@@ -10,27 +10,27 @@ namespace lambda {
     template<typename O> using Bind=typename Curry<F,(n>0)?n-1:n,OO...,O>::App;
     using App=Curry<F,n,OO...>;
     template<typename... EE> struct Expr;
-    template<typename E,typename... EE> struct Expr<E,EE...>:Bind<E>::template Expr<EE...>{};
-    template<typename E> struct Expr<E>:Bind<E> {};
+    template<typename E,typename... EE> struct Expr<E,EE...>:Bind<E>::template Expr<EE...>::App{};
+    template<typename E> struct Expr<E>:Bind<E>::App {};
   };
 
-  template<template<typename...> class F, typename O,typename... OO>
-  struct Curry<F,-1,O,OO...> {
-    template<typename P> using Bind=typename Curry<F,-1,OO...,P>::App;
-    using App=typename F<O,OO...>::App;
-    template<typename... EE> struct Expr;
-    template<typename E,typename... EE> struct Expr<E,EE...>:Bind<E>::template Expr<EE...>{};
-    template<typename E> struct Expr<E>:Bind<E> {};
-  };
+  // template<template<typename...> class F, typename O,typename... OO>
+  // struct Curry<F,-1,O,OO...> {
+  //   template<typename P> using Bind=typename Curry<F,-1,OO...,P>::App;
+  //   using App=typename F<O,OO...>::App;
+  //   template<typename... EE> struct Expr;
+  //   template<typename E,typename... EE> struct Expr<E,EE...>:Bind<E>::template Expr<EE...>{};
+  //   template<typename E> struct Expr<E>:Bind<E> {};
+  // };
 
   template<template<typename...> class F, typename O,typename... OO>
   struct Curry<F,0,O,OO...> {
     using App=typename F<O,OO...>::App;
-    // template<typename X> using Bind=typename X::DEBUG;
-    template<typename X> using Expr=typename X::DEBUG_Expr;
+    // template<typename X> using Bind=typename X::DEBUG_Bind;
+    // template<typename X> using Expr=typename X::DEBUG_Expr;
   };
 
   //////////////////////////////////////////////
   // get long bind sequences easier to write
-  template<typename O,typename... OO> using Expr=typename O::template Expr<OO...>;
+  template<typename O,typename... OO> using Expr=typename O::template Expr<OO...>::App;
 };//λ
